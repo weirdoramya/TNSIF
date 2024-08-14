73a1293681d7336eb33bb202c21177bd1b61e0c6
Ramya <Ramya@DESKTOP-954OHSH>
2024-08-14 00:23:13 -0700

    enc

--- src/com/tnsif/dayone/Encapsulationdemo.java
+++ src/com/tnsif/dayone/Encapsulationdemo.java
@@ -1,10 +1,62 @@
 package com.tnsif.dayone;
 
 public class Encapsulationdemo {
+	//team-RCB
+	private static String Playername;
+	private static int Jersyno;
+	private static int Orderno;
+	//constructor
+	public Encapsulationdemo(String playername,int jersyno,int orderno) {
+		this.Playername=playername;
+		this.Jersyno=jersyno;
+		this.Orderno=orderno;
+	}
+	
+	
+
+
+
+	public String getPlayername() {
+		return Playername;
+	}
+
+
+	public void setPlayername(String playername) {
+		this.Playername = playername;
+	}
+
+	public int getJersyno() {
+		return Jersyno;
+	}
+
+
+	public void setJersyno(int jersyno) {
+		this.Jersyno = jersyno;
+	}
+
+
+	public int getOrderno() {
+		return Orderno;
+	}
+
+
+	public void setOrderno(int orderno) {
+		this.Orderno = orderno;
+	}
+
+	@Override//source->to string->click 
+	public String toString() {
+		return "Encapsulationdemo [playername=" + Playername + ", jersyno=" + Jersyno + ", orderno=" + Orderno + "]";
+	}
+
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Encapsulationdemo obj=new Encapsulationdemo(Playername,Jersyno,Orderno);
+		obj.setPlayername("virat koli");
+		obj.setJersyno(18);
+		obj.setOrderno(1);
+		System.out.println(obj);
 	}
 
 }
